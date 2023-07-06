7078aba9deebfc4516320527fc4a01aa3752340d
UnspeakableSnow <yukkip45@gmail.com>
2023-07-06 16:31:42 +0900

    桜餅デザイン

--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,4 @@
 import * as THREE from "three";
-import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 window.addEventListener('DOMContentLoaded', init);
 let width = window.innerWidth;let height = window.innerHeight;
 function init() {
@@ -8,7 +7,7 @@ function init() {
     });
     renderer.setSize(width, height);
     const scene = new THREE.Scene();
-    renderer.setClearColor(0xccccff, 1);
+    renderer.setClearColor(0xd6adff, 1);
     const camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
     camera.position.set(0,0,10);
 
@@ -21,23 +20,24 @@ class cube_ins{
                 this.material = new THREE.MeshNormalMaterial();
                 break;
             case 1:
-                this.material = new THREE.MeshStandardMaterial({color: 0x6699FF, roughness:0.5});
+                this.material = new THREE.MeshStandardMaterial({color: 0x559944, roughness:0.5});
                 break;
         }
         this.cube = new THREE.Mesh( this.geometry, this.material );
         this.cube.position.set(posi[0],posi[1],posi[2]);
         scene.add( this.cube );
-        this.sta=(Math.random()*0.9+0.1);
+        this.sta=(Math.random()*0.3+0.1);
     }
     move(t){
-        this.cube.rotation.x+=(Math.sin(t*this.sta)+0.9)*0.05;
-        this.cube.rotation.y+=(Math.sin(t*this.sta)+0.85)*0.05;
-        this.cube.rotation.z+=Math.sin(t*this.sta+0.1*Math.PI)*0.05;
+        this.cube.rotation.x+=(Math.sin(t*this.sta)+0.9)*0.02;
+        this.cube.rotation.y+=(Math.sin(t*this.sta)+0.85)*0.02;
+        this.cube.rotation.z+=Math.sin(t*this.sta+0.1*Math.PI)*0.02;
         this.cube.scale.set(1,1,1);
     }
 }
 let cubes=[];
-for (var i=0;i<30;i++){cubes.push(new cube_ins([(Math.random()-0.5)*10,(Math.random()-0.5)*10,(Math.random()-0.9)*10],0))}
+for (var i=0;i<30;i++){
+    cubes.push(new cube_ins([(Math.random()-0.5)*8,(Math.random()-0.8)*5,(Math.random()-0.9)*10],1))}
   
 const directionalLight = new THREE.DirectionalLight(0xFFFFFF);// 平行光源
 directionalLight.position.set(1, 0.5, 1);
@@ -54,6 +54,17 @@ window.addEventListener('mousedown', (event)=>{if(looker){scene.remove(looker.ob
   });
 let time=new THREE.Clock();
 const raycaster = new THREE.Raycaster();
+var space=0.4;
+function setup(){
+for(var i=-1;i<space;i+=0.05){
+    for(var j=-1*space;j<1;j+=0.05){
+        raycaster.setFromCamera(new THREE.Vector2(i,j), camera);
+        const intersects = raycaster.intersectObjects(scene.children);
+        if(intersects.length>0){scene.remove(intersects[0].object);}
+        // if(intersects.length>0){intersects[0].object.material.color.set( 0xff0000 );}
+    }
+}}
+
 tick();
 function tick() {
     raycaster.setFromCamera(mouse, camera);
@@ -64,6 +75,7 @@ function tick() {
     else{looker=null;}
     camera.lookAt(new THREE.Vector3(0, 0, 0));
     renderer.render(scene, camera);
+    if(space!=0){setup();space=0;}
     requestAnimationFrame(tick);
 }
 }
