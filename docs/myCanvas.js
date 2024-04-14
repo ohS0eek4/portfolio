0b6038bbd97d8808968a6b55b6043c484865829a
UnspeakableSnow <yukkip45@gmail.com>
2024-04-14 21:31:55 +0900

    ぐるぐる

--- docs/myCanvas.js
+++ docs/myCanvas.js
@@ -12,7 +12,7 @@ const camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
 camera.position.set(0,0,20);
 
 class cube_ins{
-    constructor(posi,meshmode,name){
+    constructor(meshmode,name){
         this.geometry = new THREE.BoxGeometry( 1, 1, 1 );
         this.meshmode=meshmode;
         switch(this.meshmode){
@@ -24,28 +24,27 @@ class cube_ins{
                 break;
         }
         this.cube = new THREE.Mesh( this.geometry, this.material );
-        this.cube.position.set(posi[0],posi[1],posi[2]);
+        this.random=Math.random()-0.5;
+        this.cube.position.set(Math.sin(Math.random()+this.random*3+2)*11,this.random*13,Math.cos(Math.random()+this.random*3+2)*11);
         this.cube.scale.set(0,0,0);
         this.size=0;
         this.cube.name=name;
         scene.add( this.cube );
         this.sta=(Math.random()*0.3+0.1);
-        this.tosize=1;
+        this.tosize=2;
     }
     move(t){
-        this.size=this.size*0.98+(2-this.size)*0.02
-        this.cube.scale.set(this.size,this.size,this.size);
-        var range=this.cube.scale.x+(this.tosize-this.cube.scale.x)*0.15;
+        var range=this.cube.scale.x*0.9+(this.tosize-this.cube.scale.x)*0.1;
         this.cube.scale.set(range,range,range);
         this.cube.rotation.x+=(Math.sin(t*this.sta)+0.9)*0.02;
         this.cube.rotation.y+=(Math.sin(t*this.sta)+0.85)*0.02;
         this.cube.rotation.z+=Math.sin(t*this.sta+0.01)*0.02;
-        this.tosize=1;
+        this.tosize=2;
     }
 }
 let cubes=[];
 for (var i=0;i<30;i++){
-    cubes.push(new cube_ins([(Math.random()-0.5)*15,(Math.random()-0.5)*9,(Math.random()-1.1)*10],1,i));};
+    cubes.push(new cube_ins(1,i));};
 
 const directionalLight = new THREE.DirectionalLight(0xFFFFFF);// 平行光源
 directionalLight.position.set(1, 0.5, 1);
@@ -59,7 +58,7 @@ window.addEventListener('mousedown', (event)=>{
         if (window.innerWidth>800)
             new Audio("sounds/ビープ音1.mp3").play();
         console.log(String(looker.object.name)+"は生きられなかった。")}
-        cubes.push(new cube_ins([(Math.random()-0.5)*15,(Math.random()-0.5)*9,(Math.random()-1.1)*10],1,cubes.length));
+        cubes.push(new cube_ins(1,cubes.length));
     });
 window.addEventListener('resize',(event) => {
     width = window.innerWidth;height = window.innerHeight;
@@ -76,7 +75,7 @@ function tick() {
     var t=time.getElapsedTime();
     for(var i=0;i<cubes.length;i++){cubes[i].move(t)}
     const intersects = raycaster.intersectObjects(scene.children);
-    if(intersects.length>0){cubes[intersects[0].object.name].tosize=1.5;looker=intersects[0];}
+    if(intersects.length>0){cubes[intersects[0].object.name].tosize=3;looker=intersects[0];}
     else{looker=null;}
     camera.position.x= 20*Math.sin(t*0.2);
     camera.position.z= 20*Math.cos(t*0.2);
